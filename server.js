85c18d5ebb7f2741aac6b3d1df70882e771045e6
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-23 22:00:52 +0200

    Controller for users

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/users", require("./routes/users"));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
