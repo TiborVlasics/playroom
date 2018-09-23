d8509ade18a0de311e1e37661b0761edd76324b9
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-23 23:16:17 +0200

    Connection  to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
@@ -6,6 +7,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    `mongodb://${process.env.DB_USERNAME}:${
+      process.env.DB_PASSWORD
+    }@ds213053.mlab.com:13053/ticket_db`
+  )
+  .then(() => console.log("Mongodb connected"))
+  .catch(err => console.log(err));
 
 app.use("/users", require("./routes/users"));
 
