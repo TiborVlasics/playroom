21daac12cf9f4e1d803a3685340f42c584fde7dd
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-24 12:49:27 +0200

    Min length of password changed to 3

--- validation/register.js
+++ validation/register.js
@@ -20,8 +20,8 @@ module.exports = function validateRegisterInput(data) {
     errors.password = "Password field is required";
   }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be between 6 and 30 characters";
+  if (!Validator.isLength(data.password, { min: 3, max: 30 })) {
+    errors.password = "Password must be between 3 and 30 characters";
   }
 
   if (Validator.isEmpty(data.password2)) {
