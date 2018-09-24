c7187bdc6833ce496c4f488ee7949b1f632ca4b7
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-24 09:58:25 +0200

    User model and controller moved to a common user directory

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose
   .then(() => console.log("Mongodb connected"))
   .catch(err => console.log(err));
 
-app.use("/users", require("./routes/users"));
+app.use("/users", require("./users/users.controller"));
 
 const port = process.env.PORT || 5000;
 
