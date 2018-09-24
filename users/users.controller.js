6cb4558b1af0cfc56c37cb00179337d632fc2682
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-24 13:20:23 +0200

    Route for user registration

--- users/users.controller.js
+++ users/users.controller.js
@@ -2,8 +2,50 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 
+const validateRegisterInput = require("../validation/register");
+
+const User = require("./user.model");
+
+/**
+ * @route   POST /users/register
+ * @desc    Register user
+ * @access  Public
+ */
+router.post("/register", register);
+
 router.get("/", (req, res) => {
   res.json({ msg: "users works" });
 });
+
+function register(req, res) {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ name: req.body.name }).then(user => {
+    if (user) {
+      errors.name = "Name already in use";
+      res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+}
 
 module.exports = router;
