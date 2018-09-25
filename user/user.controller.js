e203082b2e7e4b91dc2a9819c202a80f2dcedf38
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-25 16:16:02 +0200

    Route for login

--- user/user.controller.js
+++ user/user.controller.js
@@ -2,17 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
 
 const userService = require("./user.service");
 
 /**
- * @route   POST /users/register
+ * @route   POST /user/register
  * @desc    Register user
  * @access  Public
  */
-router.post("/register", register);
-
-function register(req, res, next) {
+router.post("/register", (req, res, next) => {
   const { errors, isValid } = validateRegisterInput(req.body);
   if (!isValid) return res.status(400).json(errors);
 
@@ -20,6 +19,23 @@ function register(req, res, next) {
     .create(req.body)
     .then(user => res.status(200).json(user))
     .catch(err => next(err));
-}
+});
+
+/**
+ * @route   GET api/user/login
+ * @desc    Login user / returning token
+ * @access  Public
+ */
+router.post("/login", (req, res, next) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) return res.status(400).json(errors);
+
+  userService
+    .authenticate(req.body)
+    .then(user => res.status(200).json(user))
+    .catch(err =>
+      res.status(400).json({ error: "Name or password is incorrect" })
+    );
+});
 
 module.exports = router;
