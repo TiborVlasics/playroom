56e8b7c64f5715c71b467965d63418d710055c1d
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-25 16:15:53 +0200

    Authentication with jwt

--- user/user.service.js
+++ user/user.service.js
@@ -1,22 +1,46 @@
 const bcrypt = require("bcryptjs");
 const User = require("./user.model");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = {
-  create
+  create,
+  authenticate
 };
 
 async function create(user) {
-  if (await User.findOne({ name: user.name })) {
-    throw "Username already exists.";
-  }
-
+  if (await User.findOne({ name: user.name })) throw "Username already exists.";
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(user.password, salt);
-
   const newUser = new User({
     name: user.name,
     password: hash
   });
 
   return newUser.save();
+}
+
+async function authenticate(userData) {
+  const name = userData.name;
+  const password = userData.password;
+
+  user = await User.findOne({ name: name });
+
+  if (!user) throw "User not found";
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    const payload = { id: user.id, name: user.name };
+    const token = await jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "365d"
+    });
+
+    return {
+      ...payload,
+      success: true,
+      token: "Bearer " + token
+    };
+  } else {
+    throw "Password is incorrect";
+  }
 }
