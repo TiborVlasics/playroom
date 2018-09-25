b1772cf7cbfb243526def82b57b4cfc327ad2d1c
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-25 14:37:17 +0200

    Uniformized directory and file names for user handling

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose
   .then(() => console.log("Mongodb connected"))
   .catch(err => console.log(err));
 
-app.use("/users", require("./users/users.controller"));
+app.use("/users", require("./user/user.controller"));
 
 const port = process.env.PORT || 5000;
 
