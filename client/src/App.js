b8dfb97a33a9abd9ad040f464ca1b76b2afa526f
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-27 10:23:22 +0200

    Adding  bootstrap navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <Navbar />
           <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
