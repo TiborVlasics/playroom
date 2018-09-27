577dd6907b490712d40c375b255708764162ac3a
TiborVlasics <tibor.vlasics@gmail.com>
2018-09-27 09:31:44 +0200

    Login and register components with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello world</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
