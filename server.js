aa245fbc9835d82e8cb416649c9a4f83f0852e28
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-02 18:24:59 +0200

    Set up socket.io to server with simple chat socket

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const app = express();
+
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-require("dotenv").config();
 
-const app = express();
+require("dotenv").config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/user", require("./user/user.controller"));
 
 mongoose
   .connect(
@@ -18,8 +20,19 @@ mongoose
   .then(() => console.log("Mongodb connected"))
   .catch(err => console.log(err));
 
-app.use("/user", require("./user/user.controller"));
-
 const port = process.env.PORT || 5000;
+const server = app.listen(port, () => console.log(`Server running on ${port}`));
+const io = require("socket.io")(server);
 
-app.listen(port, () => console.log(`Server running on ${port}`));
+io.on("connection", function(socket) {
+  console.log("user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat", function(data) {
+    console.log("message: " + data);
+    io.emit("chat", data);
+  });
+});
