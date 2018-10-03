aa55cde30b288e66a7f618144ed0b30154a29d68
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-03 20:20:08 +0200

    Cleared app of react-bootstrap

--- client/src/components/chat/ChatTable.js
+++ client/src/components/chat/ChatTable.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import React from "react";
 
 class ChatTable extends React.Component {
-
-    render() {
-        return (
-          <Table striped hover>
-            <tbody>
-              {this.props.messages.map( message =>
-                <tr key={message.key}>
-                  <td className="name-column">{message.name}</td>
-                  <td>{message.message}</td>
-                </tr>
-              )}
-            </tbody>
-          </Table>
-        )
-      }
+  render() {
+    return (
+      <table>
+        <tbody>
+          {this.props.messages.map(message => (
+            <tr key={message.key}>
+              <td className="name-column">{message.name}</td>
+              <td>{message.message}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
-export default ChatTable
+export default ChatTable;
