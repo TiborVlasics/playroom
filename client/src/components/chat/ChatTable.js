a784ebec91fb3d9af276e52be81c47fbcc88882a
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-04 10:09:00 +0200

    Connecting chat table to redux and iterating over messages to display

--- client/src/components/chat/ChatTable.js
+++ client/src/components/chat/ChatTable.js
@@ -1,20 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { fetchMessages } from "../../actions/authActions";
+import { connect } from "react-redux";
 
 class ChatTable extends React.Component {
+  componentDidMount() {
+    this.props.fetchMessages();
+  }
+
   render() {
+    const { user } = this.props.auth;
+    const messages = this.props.messages;
+
     return (
-      <table>
-        <tbody>
-          {this.props.messages.map(message => (
-            <tr key={message.key}>
-              <td className="name-column">{message.name}</td>
-              <td>{message.message}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="chat-table">
+        {messages.map(message => (
+          <div className="chat-message" key={message._id}>
+            <img src={message.thumbnail} style={{ maxWidth: 30 }} />
+            <div className="name-column">{message.author.name}</div>
+            <div>{message.text}</div>
+          </div>
+        ))}
+      </div>
     );
   }
 }
 
-export default ChatTable;
+ChatTable.propTypes = {
+  auth: PropTypes.object.isRequired,
+  messages: PropTypes.array.isRequired,
+  fetchMessages: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  messages: state.messages
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchMessages }
+)(ChatTable);
