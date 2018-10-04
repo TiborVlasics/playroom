22f43a7728f6f70e1e182112725dea3f74b0776c
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-04 12:49:14 +0200

    Chat message html + css layout

--- client/src/components/chat/ChatTable.js
+++ client/src/components/chat/ChatTable.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { fetchMessages } from "../../actions/authActions";
 import { connect } from "react-redux";
+import classnames from "classnames";
 
 class ChatTable extends React.Component {
   componentDidMount() {
@@ -15,10 +16,20 @@ class ChatTable extends React.Component {
     return (
       <div className="chat-table">
         {messages.map(message => (
-          <div className="chat-message" key={message._id}>
-            <img src={message.thumbnail} style={{ maxWidth: 30 }} />
+          <div
+            key={message._id}
+            className={classnames("chat-message", {
+              "chat-message own-message": message.author.name === user.name
+            })}
+          >
+            <img
+              src={message.thumbnail}
+              style={{ maxWidth: 30 }}
+              alt="thumbnail"
+            />
             <div className="name-column">{message.author.name}</div>
-            <div>{message.text}</div>
+            <div class="message-text">{message.text}</div>
+            <div>{message.createdDate}</div>
           </div>
         ))}
       </div>
