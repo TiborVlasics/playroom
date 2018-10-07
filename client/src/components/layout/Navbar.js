7e55d61ad5be6b1e92d013ae7d172548e49ad919
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-07 11:36:55 +0200

    Navbar fixed to top

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -50,7 +50,7 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4 fixed-top">
         <div className="container">
           <Link className="navbar-brand" to="/">
             Gaming app
