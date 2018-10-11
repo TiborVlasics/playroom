37af38f2409aa5ec2ca5df7c3f78021194c2c23a
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-11 16:10:23 +0200

    Component for games

---
+++ client/src/components/Games.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Games extends Component {
+  render() {
+    return (
+      <div>
+        <button>New Game</button>
+        <div>Games:</div>
+      </div>
+    );
+  }
+}
+
+export default Games;
