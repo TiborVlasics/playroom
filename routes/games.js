5c5cafcc74721163270b8159fe8663f2982ca076
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-11 21:31:15 +0200

    Get running games route

--- routes/games.js
+++ routes/games.js
@@ -2,4 +2,16 @@ const express = require("express");
 const router = express.Router();
 const TicTacToe = require("../models/TicTacToe");
 
+/**
+ * @route   GET /games/
+ * @desc    Get all games that are not finished yet
+ * @access  Public
+ * TODO: make it provate
+ */
+router.get("/", (req, res) => {
+  TicTacToe.find({ isOver: false })
+    .then(game => res, status(200).json(game))
+    .catch(err => console.log(err));
+});
+
 module.exports = router;
