b2eb2b296256dbf4033102b307751cc14ce7cb09
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-12 16:52:01 +0200

    Make a card for every game in tavern

--- client/src/components/tavern/Tavern.js
+++ client/src/components/tavern/Tavern.js
@@ -15,9 +15,32 @@ class Tavern extends Component {
       <div className="container cards">
         <NewGameForm />
         <div>
-          <div>Games:</div>
+          <h1>Games:</h1>
           {this.props.games.map(game => (
-            <div key={game._id}>{game._id}</div>
+            <div
+              key={game._id}
+              className="card"
+              style={{ backgroundColor: "rgba(100, 10, 10, 0.2)" }}
+            >
+              <div className="card-body">
+                <h5 className="card-title">{game.player1.name}</h5>
+                <p className="card-text">
+                  New tictactoe game, created by {game.player1.name},{" "}
+                  <img
+                    src={game.player1.avatar}
+                    style={{ maxWidth: "70px" }}
+                    alt="user avatar"
+                  />
+                </p>
+                <h3>{game.isStarted}</h3>
+                <p className="card-text">
+                  <small className="text-text">
+                    Created: {game.createdDate}
+                  </small>
+                </p>
+                <button className="btn btn-success">Join game</button>
+              </div>
+            </div>
           ))}
         </div>
       </div>
