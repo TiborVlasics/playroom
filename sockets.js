03f39e7281c4bcb3a4c370603d3ab8634c2b3b1b
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-15 20:29:57 +0200

    Deleted commented code

--- sockets.js
+++ sockets.js
@@ -1,20 +1,8 @@
 module.exports = function(server) {
   var io = require("socket.io")(server);
 
-  // io.on("connection", function(socket) {
-  // console.log("user connected");
-
-  //   socket.on("disconnect", function() {
-  //   console.log("user disconnected");
-  //});
-
   require("./chatSocket")(io);
   require("./gameSocket")(io);
-
-  //   socket.on("user typing", function(data) {
-  //    socket.broadcast.emit("user typing", data);
-  //  });
-  //});
 
   return io;
 };
