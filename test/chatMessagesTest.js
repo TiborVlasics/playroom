1e335ec15f1bdd3231bef6f3d45f94e74df5d0d7
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-15 15:03:38 +0200

    Four more test cases for login

--- test/chatMessagesTest.js
+++ test/chatMessagesTest.js
@@ -17,13 +17,57 @@ describe("GET /api/messages/", () => {
 });
 
 describe("POST /api/user/login", () => {
-  it("body should have a token property", done => {
+  it("Request body should have a token property, on a successful login", done => {
     chai
       .request(app)
       .post("/api/user/login")
       .send({ name: "user", password: "asd" })
       .end(function(err, res) {
         expect(res.body).to.have.property("token");
+        done();
+      });
+  });
+
+  it("Request body should have an id property, on a successful login", done => {
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send({ name: "user", password: "asd" })
+      .end(function(err, res) {
+        expect(res.body).to.have.property("id");
+        done();
+      });
+  });
+
+  it("Request returns status code 400 on unsuccessful login", done => {
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send({ name: "user", password: "wrongPassword" })
+      .end(function(err, res) {
+        expect(res).to.have.status(400);
+        done();
+      });
+  });
+
+  it("Request body should not have a token property, on unsuccessful login", done => {
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send({ name: "user", password: "wrongPassword" })
+      .end(function(err, res) {
+        expect(res.body).to.not.have.property("token");
+        done();
+      });
+  });
+
+  it("Request body's name property equals to Name or password is incorrect n unsuccessful login", done => {
+    chai
+      .request(app)
+      .post("/api/user/login")
+      .send({ name: "user", password: "wrongPassword" })
+      .end(function(err, res) {
+        expect(res.body.name).to.equal("Name or password is incorrect");
         done();
       });
   });
