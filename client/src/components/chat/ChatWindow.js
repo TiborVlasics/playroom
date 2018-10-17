6615e2b5ba4c1429b5508a77b0118d63b5ab7d41
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-17 21:01:06 +0200

    ChatWindow component

--- client/src/components/chat/ChatWindow.js
+++ client/src/components/chat/ChatWindow.js
@@ -4,22 +4,64 @@ class ChatWindow extends Component {
   constructor() {
     super();
     this.state = {
+      newMessage: "",
       messages: []
     };
+
+    this.setNewMessage = this.setNewMessage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.socket.on("private", data => {
+      console.log(data);
+      this.setState({ messages: this.state.messages.concat(data.msg) });
+    });
+  }
 
   render() {
     const user = this.props.user;
+    const { messages } = this.state;
     return (
       <div id="myNav" className="overlay">
-        <a className="closebtn" onClick={this.onClick}>
-          &times;
-        </a>
-        <div className="overlay-content">{user.name}</div>
+        <a onClick={this.onClick}>&times;</a>
+        <p>messages</p>
+        <div>
+          {messages.map(msg => (
+            <p>{msg}</p>
+          ))}
+        </div>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="message"
+            type="text"
+            label="Message"
+            onChange={this.setNewMessage}
+            value={this.state.newMessage}
+            autoComplete="off"
+            autoFocus
+          />
+        </form>
       </div>
     );
+  }
+
+  setNewMessage(event) {
+    this.setState({
+      newMessage: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.socket.emit("private", {
+      to: this.props.user.id,
+      text: this.state.newMessage
+    });
+    this.setState({
+      newMessage: ""
+    });
   }
 }
 
