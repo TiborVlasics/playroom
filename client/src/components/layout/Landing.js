dc3fb08f951a132cf31d6351688bb3db68702951
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-18 21:30:18 +0200

    Delete text from landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,12 +16,7 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">some stupid app</h1>
-                <p className="lead">
-                  {" "}
-                  Gaming and social app for people who don't like challenges
-                  (with art by Banksy)
-                </p>
+                <p className="lead" />
               </div>
             </div>
           </div>
