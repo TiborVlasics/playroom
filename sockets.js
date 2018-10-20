eed7c0374095f475abc8af454cb515ffac4e8677
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-20 16:28:31 +0200

    Putting user object to socket header after verification

--- sockets.js
+++ sockets.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
-const jwtDecode = require("jwt-decode");
 
-module.exports = function(server) {
-  var io = require("socket.io")(server, {
+module.exports = function (server) {
+  const io = require("socket.io")(server, {
     transports: ["polling", "websocket"]
   });
 
@@ -10,8 +9,9 @@ module.exports = function(server) {
     if (socket.handshake.query && socket.handshake.query.token) {
       let token = socket.handshake.query.token;
       token = token.slice(7, token.length).trimLeft();
-      jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+      jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) return next(new Error("Authentication error"));
+        socket.handshake.headers.user = decoded;
         next();
       });
     } else {
