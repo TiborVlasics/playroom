e7e42c084d907893de212108709779e089889d85
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-22 19:35:34 +0200

    API route for getting a game

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const TicTacToe = require("../models/TicTacToe");
+const passport = require("passport");
+require("../config/passport")(passport);
 
 /**
  * @route   GET api/games/
@@ -13,5 +15,19 @@ router.get("/", (req, res) => {
     .then(game => res.status(200).json(game))
     .catch(err => console.log(err));
 });
+
+/**
+ * @route   GET api/games/:id
+ * @desc    Get a specific game by id
+ * @access  Private
+ */
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    TicTacToe.findOne({ id: req.query.id })
+      .then(game => res.status(200).json(game))
+      .catch(err => console.log(err));
+  });
 
 module.exports = router;
