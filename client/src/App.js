220a124450a21ca39ae98267f3451887da0a1188
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-23 20:04:59 +0200

    Making app redirect to dashboard if not route matching

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
@@ -15,7 +15,6 @@ import Dashboard from "./components/layout/Dashboard";
 import ChatRoom from "./components/chat/ChatRoom";
 import Landing from "./components/layout/Landing";
 import Tavern from "./components/tavern/Tavern";
-import TicTacToe from "./components/tavern/TicTacToe"
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -45,13 +44,15 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <div>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <SecretRoute exact path="/chat" component={ChatRoom} />
-            <SecretRoute exact path="/dashboard" component={Dashboard} />
-            <SecretRoute exact path="/tavern" component={Tavern} />
-            <SecretRoute exact path="/tictactoe" component={TicTacToe} />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <SecretRoute exact path="/chat" component={ChatRoom} />
+              <SecretRoute exact path="/dashboard" component={Dashboard} />
+              <SecretRoute exact path="/tavern" component={Tavern} />
+              <Route render={() => <Redirect to="/dashboard" />} />
+            </Switch>
           </div>
         </div>
       </Router>
