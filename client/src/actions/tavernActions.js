9dd98f6be613b337d37414cf2a5d4b31528e2084
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-23 14:30:17 +0200

    Getting game object from api, if users currentGame is not null

--- client/src/actions/tavernActions.js
+++ client/src/actions/tavernActions.js
@@ -26,7 +26,16 @@ export const getCurrentGame = () => dispatch => {
   axios
     .get("/api/user/current")
     .then(res => {
-      dispatch({ type: GET_CURRENT_GAME, payload: res.data.currentGame });
+      if (res.data.currentGame == null) {
+        dispatch({ type: GET_CURRENT_GAME, payload: res.data.currentGame });
+      } else {
+        axios
+          .get(`/api/games/${res.data.currentGame}`)
+          .then(res => {
+            dispatch({ type: GET_CURRENT_GAME, payload: res.data });
+          })
+          .catch(err => console.log(err))
+      }
     })
     .catch(err => console.log(err));
 };
