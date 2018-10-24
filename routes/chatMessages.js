b95df264b3ed0124d5587ebfa8cc9fd579ac44dd
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-24 22:08:18 +0200

    Making route private

--- routes/chatMessages.js
+++ routes/chatMessages.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const Message = require("../models/Message");
+const passport = require("passport");
 
 /**
  * @route   POST api/chat
  * @desc    Register user
- * @access  Public
+ * @access  Private
  * Returns all chat messages
- * TODO: Make it private
  */
-router.get("/", (req, res) => {
-  Message.find()
-    .sort({ createdDate: 1 })
-    .then(data => res.status(200).json(data))
-    .catch(err => console.log(err));
-});
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Message.find()
+      .sort({ createdDate: 1 })
+      .then(data => res.status(200).json(data))
+      .catch(err => console.log(err));
+  });
 
 module.exports = router;
