655815fed268c8abf64434b3efccff5fb2176646
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-24 16:23:49 +0200

    Renamed directory

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import store from "./store";
 import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
+import setAuthToken from "./helper/setAuthToken";
 import { setCurrentUser } from "./actions/authActions";
 import { getCurrentGame } from "./actions/gameActions";
 
