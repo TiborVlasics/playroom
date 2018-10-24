92dc4f0896f31a4ba25873b3ba5011f2dfd17917
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-24 11:55:38 +0200

    Making currentGame initial state an emty object

--- client/src/reducers/gameReducer.js
+++ client/src/reducers/gameReducer.js
@@ -1,6 +1,6 @@
 import { GET_CURRENT_GAME } from "../actions/types";
 
-export default function tavernReducer(state = null, action = {}) {
+export default function tavernReducer(state = {}, action = {}) {
   switch (action.type) {
     case GET_CURRENT_GAME:
       return action.payload
