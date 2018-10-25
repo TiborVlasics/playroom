50bbb690636526027c2412a222d128ed59b4043a
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-25 12:01:13 +0200

    Updating game when users joined and pushing it back to them

--- controller/ticTacToe.js
+++ controller/ticTacToe.js
@@ -2,17 +2,37 @@ const TicTacToe = require("../models/TicTacToe");
 const socketHelper = require("../helper/socketHelper")
 
 module.exports = function (io) {
-  const game = io.of("/tic-tac-toe");
+  const gameIo = io.of("/tic-tac-toe");
   let currentConnections = {};
 
-  game.on("connection", function (socket) {
+  gameIo.on("connection", function (socket) {
     const user = socket.handshake.headers.user;
 
-    socketHelper.addSocketToConnections(currentConnections, user, game, socket, "tictactoe");
+    socketHelper.addSocketToConnections(currentConnections, user, gameIo, socket, "tictactoe");
 
     socket.on("disconnect", () => {
-      socketHelper.deleteSocketFromConnections(currentConnections, user, game, socket, "tictactoe");
+      socketHelper.deleteSocketFromConnections(currentConnections, user, gameIo, socket, "tictactoe");
     });
 
+    socket.on("join me to a room please", game => {
+      socket.join(game._id)
+      let user1Connected = currentConnections.hasOwnProperty(game.player1.id);
+      let user2Connected = currentConnections.hasOwnProperty(game.player2.id);
+      if (user1Connected && user2Connected && game.isStarted === false) {
+
+        TicTacToe.findOneAndUpdate(
+          { _id: game._id },
+          {
+            $set:
+            {
+              isStarted: true,
+              nextPlayer: game.player1.id,
+              boardState: game.boardState.concat("?????????")
+            }
+          },
+          { new: true }
+        ).then(next => gameIo.to(game._id).emit("game started", next))
+      }
+    })
   })
 }
