4cc02dd542896565fbd617d077f40edd36fa9451
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-26 20:39:03 +0200

    Function that returns game object with matrix property without mutating the original object

--- helper/ticTacToe.helper.js
+++ helper/ticTacToe.helper.js
@@ -1,22 +1,18 @@
 
-module.exports = { mapGameStringToArray }
-
-/**
- * 
- * @param gameString a string representing the game eg: 'xxooxooox'
- * @returns A two dimensional array representing the game
- * 
- */
-function mapGameStringToArray(gameString) {
-  let rows = Math.sqrt(gameString.length);
-  let game = gameString.split("");
-
-  return game.reduce((acc, currValue, index) => {
+const mapGameStringToMatrix = (game) => {
+  const gameString = game.boardState[game.boardState.length - 1];
+  const rows = Math.sqrt(gameString.length);
+  const gameArray = gameString.split("");
+  const gameMatrix = gameArray.reduce((acc, currValue, index) => {
     if (index !== 0 && index % rows === 0) {
-      acc = acc.concat(["currValue"]);
+      return [...acc, [currValue]]
     } else {
       acc[acc.length - 1] = acc[acc.length - 1].concat(currValue);
     }
     return acc;
   }, [[]])
+
+  return { ...game, gameMatrix: gameMatrix }
 }
+
+module.exports = { mapGameStringToMatrix }
