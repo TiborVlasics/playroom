36d19bfb3644970f5855dd99ac5ea70b4a058fe2
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-26 20:36:36 +0200

    Rewriting test because tictactoe util functions will be pure completely

--- test/ticTacToe.helperTest.js
+++ test/ticTacToe.helperTest.js
@@ -1,185 +1,54 @@
 const chai = require("chai");
+const expect = chai.expect;
 const assert = chai.assert;
+const { mapGameStringToMatrix } = require("../helper/ticTacToe.helper");
 
-const ticTacToeHelper = require("../helper/ticTacToe.helper");
+describe("Map game's latest state to a matrix, and include it in the returned game object", function () {
 
-describe('Map game string to two dimensional array', function () {
-  it('A 9 character string should be mapped to a matrix with the length of 3',
+  let game;
+
+  beforeEach(() => {
+    game = {
+      boardState: ['?????????',
+        '?X???????',
+        '?X?O?????',
+        'OXX???XXX']
+    }
+  })
+
+  it('Game object should remain unmutated',
     () => {
-      const gameString = "xxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.equal(game.length, 3);
+      const updatedGame = mapGameStringToMatrix(game)
+      expect(game).to.not.have.property("gameMatrix")
     });
 
-  it('A 16 character string should be mapped to a matrix with the length of 4',
+  it('Updated game should have a gameMatrix property',
     () => {
-      const gameString = "xxxxxxxxxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.equal(game.length, 4);
+      const updatedGame = mapGameStringToMatrix(game)
+      expect(updatedGame).to.have.property("gameMatrix")
     });
 
-  it('A 25 character string should be mapped to a matrix with the length of 5',
+  it('Updated game should still have a boardState property',
     () => {
-      const gameString = "xxxxxxxxxxxxxxxxxxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.equal(game.length, 5);
+      const updatedGame = mapGameStringToMatrix(game)
+      expect(updatedGame).to.have.property("boardState")
     });
 
-  it('A 32 character string should be mapped to a matrix with the length of 6',
+  it('GameMatrix should have a length of 3',
     () => {
-      const gameString = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.equal(game.length, 6);
+      const updatedGame = mapGameStringToMatrix(game)
+      assert.equal(updatedGame.gameMatrix.length, 3);
     });
 
-  it("A 9 character string should be mapped to a matrix with the first item of 3 x's",
+  it("A 9 character string should be mapped to a matrix with the first row of 3 x's",
     () => {
-      const gameString = "xxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.deepEqual(game[0], ["x", "x", "x"]);
+      const updatedGame = mapGameStringToMatrix(game)
+      assert.deepEqual(updatedGame.gameMatrix[0], ["O", "X", "X"]);
     });
 
-  it("A 16 character string should be mapped to a matrix with the first item of 4 x's",
+  it("A 9 character string should be mapped to a matrix with the last row of 3 x's",
     () => {
-      const gameString = "xxxxxxxxxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.deepEqual(game[0], ["x", "x", "x", "x"]);
-    });
-
-  it("A 25 character string should be mapped to a matrix with the first item of 5 x's",
-    () => {
-      const gameString = "xxxxxxxxxxxxxxxxxxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.deepEqual(game[0], ["x", "x", "x", "x", "x"]);
-    });
-
-  it("A 32 character string should be mapped to a matrix with the first item of 6 x's",
-    () => {
-      const gameString = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-      const game = ticTacToeHelper.mapGameStringToArray(gameString)
-      assert.deepEqual(game[0], ["x", "x", "x", "x", "x", "x"]);
+      const updatedGame = mapGameStringToMatrix(game)
+      assert.deepEqual(updatedGame.gameMatrix[2], ["X", "X", "X"]);
     });
 });
-
-
-describe('Evaluate game', function () {
-  it("With a 3x3 game matrix containing x's in its first row as a parameter should return x",
-    () => {
-      const game = [
-        ["x", "x", "x"],
-        ["?", "?", "?"],
-        ["?", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "x");
-    });
-
-  it("With a 3x3 game matrix containing o's in its first row as a parameter should return o",
-    () => {
-      const game = [
-        ["o", "o", "o"],
-        ["?", "?", "?"],
-        ["?", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "o");
-    });
-
-  it("With a 3x3 game matrix containing x's in its first column as a parameter should return x",
-    () => {
-      const game = [
-        ["x", "?", "?"],
-        ["x", "?", "?"],
-        ["x", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "x");
-    });
-
-  it("With a 3x3 game matrix containing o's in its first column as a parameter should return o",
-    () => {
-      const game = [
-        ["o", "?", "?"],
-        ["o", "?", "?"],
-        ["o", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "o");
-    });
-
-  it("With a 3x3 game matrix containing x's across as a parameter should return x",
-    () => {
-      const game = [
-        ["x", "?", "?"],
-        ["?", "x", "?"],
-        ["?", "?", "x"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "x");
-    });
-
-  it("With a 3x3 game matrix containing o's across as a parameter should return o",
-    () => {
-      const game = [
-        ["o", "?", "?"],
-        ["?", "o", "?"],
-        ["?", "?", "o"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "o");
-    });
-
-  it("With a 3x3 game matrix contains 3 x's across from right as a parameter should return x",
-    () => {
-      const game = [
-        ["x", "?", "?"],
-        ["?", "x", "?"],
-        ["?", "?", "x"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "x");
-    });
-
-  it("With a 3x3 game matrix contains 3 o's across from right as a parameter should return o",
-    () => {
-      const game = [
-        ["o", "?", "?"],
-        ["?", "o", "?"],
-        ["?", "?", "o"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "o");
-    });
-
-  it("With a 3x3 game matrix contains 3 x's across from left as a parameter should return x",
-    () => {
-      const game = [
-        ["?", "?", "x"],
-        ["?", "x", "?"],
-        ["x", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "x");
-    });
-
-  it("With a 3x3 game matrix contains 3 o's across from left as a parameter should return o",
-    () => {
-      const game = [
-        ["?", "?", "o"],
-        ["?", "o", "?"],
-        ["o", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "o");
-    });
-
-  it("With a 3x3 game matrix with 2 moves as param should return continue",
-    () => {
-      const game = [
-        ["?", "?", "o"],
-        ["?", "x", "?"],
-        ["?", "?", "?"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "continue");
-    });
-
-  it("With a 3x3 game matrix with no winner should return draw",
-    () => {
-      const game = [
-        ["x", "o", "x"],
-        ["x", "x", "o"],
-        ["o", "x", "o"]];
-      const result = ticTacToeHelper.evaluateGame(game, 3);
-      assert.equal(result, "draw");
-    });
-})
