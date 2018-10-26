547baba82bd1faf92e89ee5b213cadd23394c513
TiborVlasics <tibor.vlasics@gmail.com>
2018-10-26 21:41:51 +0200

    Tests for apply move

--- test/ticTacToe.helperTest.js
+++ test/ticTacToe.helperTest.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const expect = chai.expect;
 const assert = chai.assert;
-const { mapGameArrayToMatrix, mapGameStringToArray } = require("../helper/ticTacToe.helper");
+const { mapGameArrayToMatrix, mapGameStringToArray, applyMove } = require("../helper/ticTacToe.helper");
 
 describe("Map game's latest state to a matrix, and include it in the returned game object", function () {
 
@@ -78,3 +78,43 @@ describe("Map game's latest state to an array, and include it in the returned ga
       assert.deepEqual(updatedGame.gameArray, matrix);
     });
 });
+
+describe("Apply move to a game object's gameArray",
+  function () {
+    let game;
+
+    beforeEach(() => {
+      game = {
+        player1: {
+          id: '5bd1cbb4e9f6ea38c32d6498',
+          symbol: 'X'
+        },
+        player2: {
+          id: '5bd1cbc3e9f6ea38c32d649a',
+          symbol: 'O'
+        },
+        nextPlayer: '5bd1cbc3e9f6ea38c32d649a',
+        gameArray: ["O", "X", "X", "?", "?", "?", "X", "X", "X"],
+        move: 3
+      }
+    })
+
+    it('Game object should remain unmutated',
+      () => {
+        const updatedGame = applyMove(game)
+        assert.deepEqual(game.gameArray, ["O", "X", "X", "?", "?", "?", "X", "X", "X"])
+      });
+
+    it('Updated game should still have a nextPlayer property',
+      () => {
+        const updatedGame = applyMove(game)
+        expect(updatedGame).to.have.property("nextPlayer")
+      });
+
+    it("GameArray property should be an array",
+      () => {
+        const updatedGame = applyMove(game)
+        const matrix = ["O", "X", "X", "O", "?", "?", "X", "X", "X"]
+        assert.deepEqual(updatedGame.gameArray, matrix);
+      });
+  });
