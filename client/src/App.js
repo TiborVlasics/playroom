2d5cc7216fb8720536379640c330697538650537
TiborVlasics <tibor.vlasics@gmail.com>
2018-11-11 16:16:20 +0100

    Make code look prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./helper/setAuthToken";
@@ -9,13 +14,12 @@ import { getCurrentGame } from "./actions/gameActions";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/layout/Dashboard";
 import ChatRoom from "./components/chat/ChatRoom";
-import Landing from "./components/layout/Landing";
 import Tavern from "./components/tavern/Tavern";
-import TicTacToe from "./components/tavern/TicTacToe"
-import SecretRoute from "./components/SecretRoute"
+import TicTacToe from "./components/tavern/TicTacToe";
+import SecretRoute from "./components/SecretRoute";
+import Navbar from "./components/layout/Navbar";
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -32,10 +36,10 @@ class App extends Component {
           <Navbar />
           <div>
             <Switch>
-              <Route exact path="/" component={Landing} />
+              <Route exact path="/" component={Login} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <SecretRoute exact path="/chat" component={ChatRoom} />
+              <SecretRoute path="/chat" component={ChatRoom} />
               <SecretRoute exact path="/dashboard" component={Dashboard} />
               <SecretRoute exact path="/tavern" component={Tavern} />
               <SecretRoute exact path="/tictactoe/:id" component={TicTacToe} />
