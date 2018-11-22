f1af46702b9b895804d48c4b049e530903bd16da
TiborVlasics <tibor.vlasics@gmail.com>
2018-11-22 17:14:50 +0100

    Add layout to game cards

--- client/src/components/tavern/GameList.js
+++ client/src/components/tavern/GameList.js
@@ -8,14 +8,10 @@ const GameList = ({ games, currentGame, joinGame, deleteGame }) => {
 
         if (game.hasOwnProperty("player2")) {
           player2 = (
-            <p className="card-text">
-              player2: {game.player2.name}
-              <img
-                src={game.player2.avatar}
-                style={{ maxWidth: "40px" }}
-                alt="user avatar"
-              />
-            </p>
+            <div className="card-player">
+              {game.player2.name}
+              <img src={game.player2.avatar} alt="user avatar" />
+            </div>
           );
         }
 
@@ -27,41 +23,31 @@ const GameList = ({ games, currentGame, joinGame, deleteGame }) => {
             }
           >
             <div className="card-header">
-              {game._id === currentGame._id ? (
-                <button onClick={() => deleteGame(game)} className="btn-delete">
-                  Cancel game
-                </button>
-              ) : null}
               <h5 className="card-title">{game.kind}</h5>
-            </div>
-            <div className="card-body">
-              <p className="card-text">
-                player1: {game.player1.name}
-                <img
-                  src={game.player1.avatar}
-                  style={{
-                    maxWidth: "40px",
-                    borderRadius: "100%",
-                    marginLeft: "10px"
-                  }}
-                  alt="user avatar"
-                />
-              </p>
-              {player2}
-              <h3>{game.isFull}</h3>
-              <p className="card-text">
-                <small className="text-text">Created: {game.createdDate}</small>
-              </p>
               <button
                 className={
                   currentGame.hasOwnProperty("_id") || game.isFull
                     ? "btn btn-success hidden-button"
-                    : "btn btn-success"
+                    : "btn btn-success join-button"
                 }
                 onClick={() => joinGame(game)}
               >
                 Join game
               </button>
+              {game._id === currentGame._id ? (
+                <button onClick={() => deleteGame(game)} className="btn-delete">
+                  Cancel game
+                </button>
+              ) : null}
+            </div>
+            <div className="card-body">
+              <div>Players:</div>
+              <div className="card-player">
+                {game.player1.name}
+                <img src={game.player1.avatar} alt="user avatar" />
+              </div>
+              {player2}
+              <div className="card-created">Created: {game.createdDate}</div>
             </div>
           </div>
         );
