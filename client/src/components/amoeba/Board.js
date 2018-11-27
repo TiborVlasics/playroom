a6109f6d07b95236b761ef4fc221e0c9015afda2
TiborVlasics <tibor.vlasics@gmail.com>
2018-11-27 12:56:04 +0100

    Add colors to moves when replaying

--- client/src/components/amoeba/Board.js
+++ client/src/components/amoeba/Board.js
@@ -5,9 +5,13 @@ export default function Board({ replayBoard, game, auth, move }) {
   return (
     <div className="game-board">
       {replayBoard
-        ? replayBoard
-            .split("")
-            .map(value => <div>{value !== "?" ? value : null}</div>)
+        ? replayBoard.split("").map(value => {
+            if (value === "X") {
+              return <div className="x">{value !== "?" ? value : null}</div>;
+            } else {
+              return <div>{value !== "?" ? value : null}</div>;
+            }
+          })
         : game.isStarted
         ? game.boardState[game.boardState.length - 1]
             .split("")
